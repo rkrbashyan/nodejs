08c9d6f73ce94be713f2caf3b4f94203d90b8544
ruben <krbashyan.r@gmail.com>
2018-11-10 14:08:49 +0200

    mongodb way

--- utils/database.js
+++ utils/database.js
@@ -1,20 +1,29 @@
-const Sequelize = require("sequelize")
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "rubenkrbashyan", {
-  dialect: "mysql",
-  host: "localhost"
-})
+let _db;
 
-module.exports = sequelize
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
-/* // create connection pool
-const mysql = require("mysql2")
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "rubenkrbashyan"
-})
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
 
-module.exports = pool.promise()
- */
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
